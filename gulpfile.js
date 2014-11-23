8c0ad083123c8a28bc35f28129a5dfe8ba101699
Phil Mander <philip.mander@gmail.com>
2014-11-23 18:20:23 +0100

    New dependencies

--- gulpfile.js
+++ gulpfile.js
@@ -3,9 +3,11 @@ var jshint = require('gulp-jshint');
 var jasmine = require('gulp-jasmine');
 var uglify = require('gulp-uglify');
 var browserify = require('browserify');
+var watchify = require('watchify');
 var reactify = require('reactify');
 var source = require('vinyl-source-stream');
 var del = require('del');
+var alert = require('./gulp/gulp-alert');
 
 gulp.task('clean', function () {
     return del('./dist/**/*.*');
@@ -39,10 +41,34 @@ gulp.task('package', [ 'test', 'clean' ], function() {
         entries: './lib/index.js',
         standalone: "calc"
     })
+    //bundler.ignore('react');
     .transform(reactify)
     .bundle()
     .pipe(source('calc.js'))
+    .pipe(alert('Welcome to the calculator!'))
     .pipe(gulp.dest('./dist'));
+});
+
+//start watching for changes
+gulp.task('watch', function() {
+    var bundler = watchify(browserify({
+        entries: './lib/index.js',
+        standalone: "calc"
+    }), watchify.args);
+
+    //bundler.ignore('react');
+    bundler.transform(reactify);
+
+    bundler.on('update', rebundle);
+
+    function rebundle() {
+        return bundler.bundle()
+            .pipe(source('calc.js'))
+            .pipe(alert('Welcome to the calculator! (built by watch)'))
+            .pipe(gulp.dest('./dist'));
+    }
+
+    return rebundle();
 });
 
 gulp.task('default', ['package']);
